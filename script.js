d34fb666217791d333d2fcc5b114ef37d4ee94c0
kahodayoon <kaho9707@gmail.com>
2023-10-18 23:04:44 +0900

    SPトップ編集2

---
+++ script.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('.c-menu').click(function () {
+    $('.p-foodmenu').toggleClass('open');
+    $('.p-foodmenu--background').toggleClass('open');
+  });
+  $('.c-menu--close').click(function () {
+    $('.p-foodmenu').toggleClass('open');
+    $('.p-foodmenu--background').toggleClass('open');
+  });
+});
