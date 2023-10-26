889b1cde319d9aefb61178121c97a1723a0235d1
kahodayoon <kaho9707@gmail.com>
2023-10-26 19:42:52 +0900

    トップページ

--- script.js
+++ script.js
@@ -7,4 +7,17 @@ $(function () {
     $('.p-foodmenu').toggleClass('open');
     $('.p-foodmenu--background').toggleClass('open');
   });
+  $(window).resize(function () {
+    // 画面幅が変更されたときに実行させたい処理内容
+    $('.p-foodmenu').removeClass('open');
+    $('.p-foodmenu--background').removeClass('open');
+  });
+  // $('.js-menu').click(function () {
+  //   $('.js-foodmenu').toggleClass('open');
+  //   $('.js-foodmenu--background').toggleClass('open');
+  // });
+  // $('.js-menu--close').click(function () {
+  //   $('.js-foodmenu').toggleClass('open');
+  //   $('.js-foodmenu--background').toggleClass('open');
+  // });
 });
