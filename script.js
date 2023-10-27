6634e4100a5eedc99b2bfdb9f83f514de1dcf6c6
kahodayoon <kaho9707@gmail.com>
2023-10-28 08:50:49 +0900

    231028

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 $(function () {
-  // $('.c-menu').click(function () {
-  //   $('.p-foodmenu').toggleClass('open');
-  //   $('.p-foodmenu--background').toggleClass('open');
-  // });
-  // $('.c-menu--close').click(function () {
-  //   $('.p-foodmenu').toggleClass('open');
-  //   $('.p-foodmenu--background').toggleClass('open');
-  // });
-  // $(window).resize(function () {
-  //   // 画面幅が変更されたときに実行させたい処理内容
-  //   $('.p-foodmenu').removeClass('open');
-  //   $('.p-foodmenu--background').removeClass('open');
-  // });
   $('.js-foodmenu-title--sp').click(function () {
     $('.js-foodmenu').toggleClass('is-open');
     $('.js-foodmenu--background').toggleClass('is-open');
@@ -19,5 +6,10 @@ $(function () {
   $('.js-foodmenu--close').click(function () {
     $('.js-foodmenu').toggleClass('is-open');
     $('.js-foodmenu--background').toggleClass('is-open');
+  });
+  $(window).resize(function () {
+    // 画面幅が変更されたときに実行させたい処理内容
+    $('.js-foodmenu').removeClass('is-open');
+    $('.js-foodmenu--background').removeClass('is-open');
   });
 });
