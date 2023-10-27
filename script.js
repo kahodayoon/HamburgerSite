332632424df84f652609ed18f4ec2f7eba589140
kahodayoon <kaho9707@gmail.com>
2023-10-27 18:57:07 +0900

    20231027

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 $(function () {
-  $('.c-menu').click(function () {
-    $('.p-foodmenu').toggleClass('open');
-    $('.p-foodmenu--background').toggleClass('open');
-  });
-  $('.c-menu--close').click(function () {
-    $('.p-foodmenu').toggleClass('open');
-    $('.p-foodmenu--background').toggleClass('open');
-  });
-  $(window).resize(function () {
-    // 画面幅が変更されたときに実行させたい処理内容
-    $('.p-foodmenu').removeClass('open');
-    $('.p-foodmenu--background').removeClass('open');
-  });
-  // $('.js-menu').click(function () {
-  //   $('.js-foodmenu').toggleClass('open');
-  //   $('.js-foodmenu--background').toggleClass('open');
+  // $('.c-menu').click(function () {
+  //   $('.p-foodmenu').toggleClass('open');
+  //   $('.p-foodmenu--background').toggleClass('open');
   // });
-  // $('.js-menu--close').click(function () {
-  //   $('.js-foodmenu').toggleClass('open');
-  //   $('.js-foodmenu--background').toggleClass('open');
+  // $('.c-menu--close').click(function () {
+  //   $('.p-foodmenu').toggleClass('open');
+  //   $('.p-foodmenu--background').toggleClass('open');
   // });
+  // $(window).resize(function () {
+  //   // 画面幅が変更されたときに実行させたい処理内容
+  //   $('.p-foodmenu').removeClass('open');
+  //   $('.p-foodmenu--background').removeClass('open');
+  // });
+  $('.js-foodmenu-title--sp').click(function () {
+    $('.js-foodmenu').toggleClass('.is-open');
+    $('.js-foodmenu--background').toggleClass('.is-open');
+  });
+  $('.js-foodmenu--close').click(function () {
+    $('.js-foodmenu').toggleClass('.is-open');
+    $('.js-foodmenu--background').toggleClass('.is-open');
+  });
 });
